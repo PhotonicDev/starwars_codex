ecee69cfbcb4e59e1277b3cd16a3f5a1123fa41a
Nerijus Pamedytis <studentas.nerijus@gmail.com>
2023-09-09 11:30:13 +0200

    adding people list

---
+++ src/app/pages/people-list/people-list.component.ts
@@ -0,0 +1,27 @@
+import { CommonModule } from '@angular/common'
+import { Component } from '@angular/core'
+import { Store } from '@ngxs/store'
+import { PeopleState } from 'src/app/modules/people/people.state'
+import { MatListModule } from '@angular/material/list'
+import { Observable, first } from 'rxjs'
+import { Person } from 'src/app/modules/people/people'
+import { RouterModule } from '@angular/router'
+import { GetPeople } from 'src/app/modules/people/people.actions'
+
+@Component({
+  standalone: true,
+  selector: 'app-people-list',
+  templateUrl: './people-list.component.html',
+  styleUrls: ['./people-list.component.scss'],
+  imports: [CommonModule, MatListModule, RouterModule],
+})
+export class PeopleListComponent {
+  people$: Observable<Person[]> = this.store.select(PeopleState.people)
+  currentPage$: Observable<number> = this.store.select(PeopleState.currentPage)
+  constructor(private readonly store: Store) {}
+  loadMoreItems() {
+    this.currentPage$.pipe(first()).subscribe(page => {
+      this.store.dispatch(new GetPeople(page + 1))
+    })
+  }
+}
