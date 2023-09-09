04a77c8f2d84aa1bee45ddbf2a1a008c3692ef2d
Nerijus Pamedytis <studentas.nerijus@gmail.com>
2023-09-09 11:31:01 +0200

    Adding person details page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule, inject } from '@angular/core'
+import {
+  ActivatedRouteSnapshot,
+  RouterModule,
+  RouterStateSnapshot,
+  Routes,
+} from '@angular/router'
+import { PeopleListComponent } from './pages/people-list/people-list.component'
+import { Store } from '@ngxs/store'
+import { GetPeople, GetPerson } from './modules/people/people.actions'
+import { PersonDetailsComponent } from './pages/person-details/person-details.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PeopleListComponent,
+    resolve: { data: () => inject(Store).dispatch(new GetPeople(1)) },
+  },
+  {
+    path: ':uid',
+    component: PersonDetailsComponent,
+    resolve: {
+      data: (route: ActivatedRouteSnapshot) =>
+        inject(Store).dispatch(new GetPerson(route.params['uid'])),
+    },
+  },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/pages/person-details/person-details.component.ts
@@ -0,0 +1,29 @@
+import { CommonModule } from '@angular/common'
+import { Component, OnInit } from '@angular/core'
+import { Store } from '@ngxs/store'
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button'
+import { ActivatedRoute, RouterModule } from '@angular/router'
+import { filter, map, switchMap } from 'rxjs/operators'
+import { PeopleState } from 'src/app/modules/people/people.state'
+
+@Component({
+  standalone: true,
+  selector: 'app-person-details',
+  templateUrl: './person-details.component.html',
+  styleUrls: ['./person-details.component.scss'],
+  imports: [CommonModule, MatButtonModule, MatIconModule, RouterModule],
+})
+export class PersonDetailsComponent implements OnInit {
+  person$ = this.activatedRoute.paramMap.pipe(
+    map(params => params.get('uid')),
+    filter(Boolean),
+    switchMap((uid: string) => this.store.select(PeopleState.personById(uid)))
+  )
+  constructor(
+    private readonly activatedRoute: ActivatedRoute,
+    private readonly store: Store
+  ) {}
+
+  ngOnInit(): void {}
+}
